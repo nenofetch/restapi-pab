29c41fc75e7c9cbab4f4a965a067758fff027a8f
Neno Arisma <nenoarisma@gmail.com>
2024-06-13 16:36:11 +0700

    chore: bumping dependencies version

--- apk/app/src/main/java/com/pab/android_client/ApiClient.java
+++ apk/app/src/main/java/com/pab/android_client/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     //link base url disesuaikan dengan Alamat ip masing-masing,
-    private static final String BASE_URL = "http://192.168.0.224:3000/webserver/";
+    private static final String BASE_URL = "http://192.168.225.60:3000/webserver/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
